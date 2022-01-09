2c095ba035f9231d0f587749c0af74719f02d738
George Beshay <90455303+GeorgeBeshay@users.noreply.github.com>
2022-01-10 00:46:11 +0200

    Delete Final/Project Main directory

--- Final/Project Main/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-  Import_Data_Beginner(Beginner_Board_3D,Beginner_Board_Colour_3D,&URB_GS_PI_Current,1,&Board_Number);
-                Copy_3D_To_2D(6,6,13,Board_Number,Beginner_Board_3D,Beginner_Board);
-                Copy_3D_To_2D(6,6,13,Board_Number,Beginner_Board_Colour_3D,Beginner_Board_Colour);
-                printf("Reached........\n");
